28d3a7c7e83b9e9492a28135f6f677bc3c25821c
Juan Sebastian Mora Rodriguez <juansebas.mora@urosario.edu.co>
2025-03-26 12:31:00 -0500

    Delete BankingSystem - copia directory

--- BankingSystem - copia/app/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-import os
-
-app = Flask(__name__, template_folder=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates'))
-
-# Importa las rutas y la API
-from app import api, routes
